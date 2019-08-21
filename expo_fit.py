3c3b4616b2222a6322dbbf7898a296f233c960ae
Zhenning Liu <54356672+IceColdRefreshment@users.noreply.github.com>
2019-08-21 19:48:50 +0100

    Add files via upload

--- expo_fit.py
+++ expo_fit.py
@@ -39,15 +39,19 @@ def rho1(s,height=height,k1=k1,k2=k2,peak_0=peak_0):
         ret[s>=peak_0] = height/k1 - height/k2 * np.exp(k2*(peak_0-s)) + height/k2
         return ret
 
-def s1(rho):
-    crit = height/k1
-    if(isinstance(rho,float)):
-        if(rho < crit):
+def s1(rho,height=height,k1=k1,k2=k2,peak_0=peak_0):
+    """
+    s(rho). The inverse function of rho(s). It is used to generate importance sampling on s.
+    rho: ndArray or float, rho = integral of g(s)
+    """
+    crit = height/k1                                                                        # value of rho at the peak
+    if(isinstance(rho,float)):                                                              # Piecewise function
+        if(rho < crit):                                                                    
             return np.log(k1*rho/height)/k1 + peak_0
         else:
             return -np.log(-k2*(rho-crit)/height)/k2 + peak_0
     else:
-        ret = np.zeros(len(rho),dtype=float)
+        ret = np.zeros(len(rho),dtype=float)                                                # The array of s values
         ret[rho < crit] = np.log(k1*rho[rho<crit]/height)/k1 + peak_0
         ret[rho > crit] = -np.log(-k2*(rho[rho>crit]-crit-height/k2)/height)/k2 + peak_0
         return ret
