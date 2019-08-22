3357316eea2037f08df86d2607124c13fc0d3305
Zhenning Liu <54356672+IceColdRefreshment@users.noreply.github.com>
2019-08-22 17:05:29 +0100

    Add files via upload

--- integrand_multipeaks.py
+++ integrand_multipeaks.py
@@ -4,9 +4,14 @@ from matplotlib import pyplot as plt
 def f(s):
     m=1.0
     v0=1.0
+    #factor0 = np.sqrt(2.0/np.pi)/(v0**3 * m**4)                            # Scalar coefficient
+    factor0 = 1.0
+    factor1 = (s - 4.0*(m**2)) * np.exp(-(s-4.0*m**2)/(2.0*m**2*v0**2))    # velocity and its distribution
+    factor2 = 1.0 / (s*( (s - 30.0)**2 +0.1 ))                             # matrix element
+    '''
     factor0 = 1.0- (4.0*(m**2) / s)
     factor1 = np.exp(-1.0 * (s-4.0*(m**2)) / (2.0*(m**2)*(v0**2)))
-    factor2 = 1.0 / ( (s - 30.0)**2 +0.1 )
+    factor2 = 1.0 / ( (s - 30.0)**2 +0.1 )'''
     return factor0*factor1*factor2
 
 if __name__=="__main__":
